9e44b6712c6a9093846260368c14f5843781b632
snsaleh1 <snsaleh11@gmail.com>
2019-11-22 13:55:13 -0600

    budget event listener set

--- js/app.js
+++ js/app.js
@@ -15,5 +15,55 @@ class UI {
     this.itemList = [];
     this.itemID = 0;
   }
+  //submit budget method
+  submitBudgetForm(){
+    const value = this.budgetInput.value;
+    if(value==='' || value <0){
+      this.budgetFeedback.classList.add('showItem');
+      this.budgetFeedback.innerHTML=`<p>vallue cannot be empty or negative</p>`;
+      const self= this;
+      console.log(this);
+
+      setTimeout(function(){
+        console.log(this);
+        console.log(self);
+        this.budgetFeedback.classList.remove('showItem');
+      }, 4000);
+    }
+    else {
+      this.budgetAmount.textContent = value;
+      this.budgetInput.value = '';
+      this.showBalance();
+    }
+  }
+  //show balance
+  showBalance() {
+    console.log(`Show me the money!`);
+  }
 }
 
+function eventListeners(){
+  const budgetForm = document.getElementById('budget-form');
+  const expenseForm = document.getElementById('expense-form');
+  const expenseList = document.getElementById('expense-list');
+
+
+  //new instance of the UI Class
+  const ui = new UI()
+
+  // budget form submit
+  budgetForm.addEventListener('submit', function(event){
+    event.preventDefault();
+    ui.submitBudgetForm();
+  })
+  // expense form submit
+  expenseForm.addEventListener('submit', function(event){
+    event.preventDefault();
+  })
+  // budget form submit
+  expenseList.addEventListener('submit', function(){})
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+  eventListeners();
+})
